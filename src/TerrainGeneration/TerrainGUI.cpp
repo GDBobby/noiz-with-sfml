00084a11b64f84216a8e1c85562ae71a7d002b4c
GDBobby <corey.w108@gmail.com>
2023-09-26 02:06:53 -0500

    need options for other noise types
    minor change so that some generation types dont overflow

--- src/TerrainGeneration/TerrainGUI.cpp
+++ src/TerrainGeneration/TerrainGUI.cpp
@@ -9,7 +9,7 @@ TerrainGUI::TerrainGUI(uint16_t image_size, sf::Font& font) :
 	amplitude{ "Amplitude", sf::Vector2f{ static_cast<float>(image_size) + 400.f, 400.f }, Perlin_Control::BT_iterary, font },
 	frequency{ "Frequency", sf::Vector2f{ static_cast<float>(image_size) + 400.f, 500.f }, Perlin_Control::BT_iterary, font },
 	regenerateSeed{ "new seed", sf::Vector2f{static_cast<float>(image_size) + 400.f, 20.f}, 24, sf::Color{20,20,20,255}, sf::Color::Red, font, 2.f },
-	generationType{ sf::Vector2f{ static_cast<float>(image_size) + 600.f, 100.f }, 24, font, 100}
+	generationType{ sf::Vector2f{ static_cast<float>(image_size) + 600.f, 100.f }, 24, font, 200}
 {
 	title.setTextColor(sf::Color::White);
 	generationType.pushback("Basic");
